9be48124b4be04cd83c9199739cf7695f65acf20
Hayden <haydenhartman10@yahoo.com>
2023-05-19 17:25:35 -0400

    started adding block class

---
+++ src/main/java/net/hayden/shung/block/ModBlocks.java
@@ -0,0 +1,10 @@
+package net.hayden.shung.block;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModBlocks {
+    //public static final DefferedRegister<Block> BLOCKS =
+    //        DefferedRegister.create(ForgeRegistries)
+}
