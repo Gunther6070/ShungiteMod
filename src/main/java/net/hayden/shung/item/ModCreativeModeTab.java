cb3896f6de96781f613ac9b1e3992b7a2bf600fa
Hayden <haydenhartman10@yahoo.com>
2023-05-25 16:31:54 -0400

    Added textures and unversioned files

---
+++ src/main/java/net/hayden/shung/item/ModCreativeModeTab.java
@@ -0,0 +1,14 @@
+package net.hayden.shung.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class ModCreativeModeTab {
+    public static final CreativeModeTab SHUN_TAB = new CreativeModeTab("shuntab") {
+        @Override
+        public @NotNull ItemStack makeIcon() {
+            return new ItemStack(ModItems.SHUNGITE.get());
+        }
+    };
+}
